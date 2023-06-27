17d6624de344acb21e56ad2b8eb0c40e65b5b873
lucasjoliveau <joliveau.lucas@gmail.com>
2023-06-27 15:27:01 +0200

    Initial commit

---
+++ custom_theme/theme_odoo/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'odoo_theme_test',
+    'version': '1.0',
+    'category': 'Theme/website',
+    'author': 'lucasjoliveau',
+    'depends': ['website'],
+    'data': ['views/odoo_theme.xml'],
+    'installable': True,
+    'auto_install': False
+}
